2ba06cd10ecb4951fbd04b3893cd46fb2c1c4179
ryutaegi <gaonnurirtg@hanmail.net>
2022-10-07 20:08:15 +0900

    스크롤 섹션 모션 구현완료

--- script.js
+++ script.js
@@ -139,9 +139,9 @@ if (matchMedia("screen and (max-width: 768px)").matches) {
   });
 
   var swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
-    slidesPerGroup: 2,
+    slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
@@ -281,12 +281,17 @@ if(loginn["로그인유무"]==1)
   const element = document.getElementById('log');
   element.innerHTML 
   = '&nbsp&nbsp&nbsp&nbsp로그아웃&nbsp&nbsp&nbsp&nbsp';
+  const element1 = document.getElementById('log1');
+  element1.innerHTML 
+  = '로그아웃';
 }
 else{
   const element = document.getElementById('log');
   element.innerHTML 
   = '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp로그인&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp';
-
+  const element1 = document.getElementById('log1');
+  element1.innerHTML 
+  = '로그인';
 
 }
 function logclick(){
@@ -306,4 +311,49 @@ location.href = "./login.html";
 
 // Json Object를 저장하기
 
+}
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+window.onload = function(){
+  const elm = document.querySelectorAll('.section');
+  const elmCount = elm.length;
+  elm.forEach(function(item, index){
+    item.addEventListener('mousewheel', function(event){
+      event.preventDefault();
+      let delta = 0;
+
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+      } 
+      else if (event.detail)
+          delta = -event.detail / 3;
+
+      let moveTop = window.scrollY;
+      let elmSelector = elm[index];
+
+      // wheel down : move to next section
+      if (delta < 0){
+        if (elmSelector !== elmCount-1){
+          try{
+            moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
+      
+      // wheel up : move to previous section
+      else{
+        if (elmSelector !== 0){
+          try{
+            moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
+
+      const body = document.querySelector('html');
+      window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+    });
+  });
 }
